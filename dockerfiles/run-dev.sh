857120f30e27727f1fff509c4452db55e26c94e0
Sapkota Sushma <susma.sapkota5@gmail.com>
2024-09-02 21:08:46 +0200

    chore: Add Docker compose commands to start and stop services in bash scripts

---
+++ dockerfiles/run-dev.sh
@@ -0,0 +1,3 @@
+docker-compose -f docker-compose.yaml up -d
+
+// docker-compose -f docker-compose.kafka.yaml up -d
---
+++ dockerfiles/stop-dev.sh
@@ -0,0 +1,4 @@
+docker-compose -f docker-compose.yaml down -v
+
+// docker-compose -f docker-compose.kafka.yaml down -v
+
