87d6fbb5986864ee75369b0b3e2db9bb991fab30
Sapkota Sushma <susma.sapkota5@gmail.com>
2024-09-10 10:23:17 +0200

    Update IProductInputPort to include customerId in product operations
    - Modified methods to include customerId for identifying associated products.
    - Updated saveProduct, getAllProducts, getProductById, updateProduct, and deleteProduct methods to accept customerId.
    - Adjusted method signatures to reflect changes in handling product-related operations by customer.

--- src/main/java/com/example/productservice/core/port/input/IProductInputPort.java
+++ src/main/java/com/example/productservice/core/port/input/IProductInputPort.java
@@ -7,9 +7,9 @@ import java.io.IOException;
 import java.util.List;
 
 public interface IProductInputPort {
-    Product saveProduct(Product product, MultipartFile imageFile) throws IOException;
-    List<Product> getAllProducts();
-    Product getProductById(String id);
-    Product updateProduct(String id, Product updatedProduct, MultipartFile imageFile) throws IOException;
-    void deleteProduct(String id);
+    Product saveProduct(String customerId, Product product, MultipartFile imageFile) throws IOException;
+    List<Product> getAllProducts(String customerId);
+    Product getProductById(String customerId, String productId);
+    Product updateProduct(String customerId, String productId, Product updatedProduct, MultipartFile imageFile) throws IOException;
+    void deleteProduct(String customerId, String productId);
 }
